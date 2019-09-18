95aee16e3d339e8c73586618a57b8efc2a503c9c
mjzefeldt <mjzefeldt@gmail.com>
2019-09-18 14:52:53 -0500

    devise auth basic

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
   namespace :v1, defaults: { format: 'json' } do # routing for api v1
     get 'frameworks', to: 'frameworks#index'
   end
 
   get '*page', to: 'static#index', constraints: ->(req) do
-  !req.xhr? && req.format.html?  # non-ajax and html Mime type -> forward to StaticController 
-end
+    !req.xhr? && req.format.html?  # non-ajax and html Mime type -> forward to StaticController 
+  end
+  
   root 'static#index' # forward root to StaticController
+
 end
