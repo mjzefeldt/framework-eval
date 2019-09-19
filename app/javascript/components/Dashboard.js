11f7227a0e0fbaacb315300d77bb33bc0e90e918
mjzefeldt <mjzefeldt@gmail.com>
2019-09-19 13:24:06 -0500

    create framework obj with vote totals

--- app/javascript/components/Dashboard.js
+++ app/javascript/components/Dashboard.js
@@ -26,6 +26,7 @@ class Dashboard extends Component {
   }
 
   handleVoteClick(e) {
+    // e.value // or something like that - take in id of element passing in
     const bodyData = {framework: {id: 10270250}};
 
     e.preventDefault();
@@ -44,8 +45,19 @@ class Dashboard extends Component {
   }
 
   render () {
-    console.log(this.props.voteTotals, '<<< voteTotals')
-    const frameworks = this.props.frameworks;
+    const frameworks = this.props.frameworks.reduce((acc, cur) => {
+      const newObj = {
+        id: cur.id,
+        name: cur.name,
+        size: cur.size,
+        watchers_count: cur.watchers_count,
+        open_issues_count: cur.open_issues_count
+      }
+      const countObj = this.props.voteTotals.find(f => f.framework_id === cur.id);
+      countObj.vote_total? newObj['vote_count'] = countObj.vote_total : newObj['vote_count'] = 0;
+      acc.push(newObj);
+      return acc;
+    }, []);
 
     const frameWorkList = frameworks.map((framework) => 
       <div key={framework.id} className="single-f-wrapper">
@@ -54,7 +66,7 @@ class Dashboard extends Component {
         <div>Size: {framework.size}</div>
         <div>Watchers: {framework.watchers_count}</div>
         <div>Open Issues: {framework.open_issues_count}</div>
-        <div>Total Votes: {framework.vote_total}</div>
+        <div>Total Votes: {framework.vote_count}</div>
       </div> 
     );
     return (
